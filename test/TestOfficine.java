66f99be82dfa17f9276dff965907e68edee5bcc7
olivierfalahi <olivier.falahi@gmail.com>
2025-12-08 12:24:05 +0100

    Remove unused import of AtomicInteger in TestOfficine

--- test/TestOfficine.java
+++ test/TestOfficine.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestOfficine {
